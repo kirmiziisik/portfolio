40660cb964ff7e1d90c6d991a211748e24f2af0c
kirmiziisik <usak.sezer@gmail.com>
2022-10-26 19:17:59 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const tabTitles = document.querySelector(".tab-titles");
+const tabLinks = document.querySelectorAll(".tab-links");
+const contents = document.querySelectorAll(".tab-contents");
+const sideMenu = document.getElementById("side-menu");
+const iconX = document.querySelector(".fa-xmark");
+const iconBars = document.querySelector(".fa-bars");
+
+tabTitles.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+
+  if (id) {
+    tabLinks.forEach((link) => {
+      link.classList.remove("active-link");
+    });
+    e.target.classList.add("active-link");
+    contents.forEach((content) => {
+      content.classList.remove("active-tab");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active-tab");
+  }
+});
+
+iconBars.addEventListener("click", () => {
+  sideMenu.style.right = "0";
+});
+
+iconX.addEventListener("click", () => {
+  sideMenu.style.right = "-200px";
+});
